49628d30457f70a48e9a4aac9a1b93ec6f945867
Nik <lolwut-lol@yandex.ru>
2021-02-21 09:28:41 +0300

    Documentation for what the PS scripts do

--- create_mo.ps1
+++ create_mo.ps1
@@ -1,10 +1,17 @@
+# This script goes through every locale directory and generates .mo files required for gettext to function
+
+# Get all locale directories
 $locale_folders = Get-ChildItem -Path .\locales -Directory
+# Cycle over them
 foreach ($folder in $locale_folders) {
+	# Get a list of .po files in the locale folder
 	$po = Get-ChildItem -Path "$($folder.FullName)\LC_MESSAGES\" -filter *.po
-	echo $po
+	# Cycle over them
 	foreach ($file in $po) {
+		# Generate the respective .mo file
 		python .\i18n\msgfmt.py -o "$($folder.FullName)\LC_MESSAGES\$($file.baseName).mo" $file.FullName
 	}
 }
+
 echo "Press any key to finish..."
 $x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
--- create_templates.ps1
+++ create_templates.ps1
@@ -1,15 +1,28 @@
+# This script generates .pot templates, resaves them with proper encoding and places .po copies in every locale directory
+
+# Get all .py files
 $py_files = Get-ChildItem -Path .\bot -filter *.py
+# Cycle over them
 foreach ($py_file in $py_files) {
+	# Generate .pot files
 	python .\i18n\pygettext.py -d $py_file.baseName -o ".\locales\$($py_file.baseName).pot" $py_file.FullName
 }
+# Get all .pot files
 $pot_files = Get-ChildItem -Path .\locales -filter *.pot
+# Get all locale folders
 $locale_folders = Get-ChildItem -Path .\locales -Directory
+# Cycle over .pot files
 foreach ($pot_file in $pot_files) { 
+	# Open the file and replace the automatically detected encoding
 	$content = (Get-Content -Raw $pot_file.FullName) -replace 'cp1251','UTF-8'
+	# Resave in UTF-8
 	[IO.File]::WriteAllLines($pot_file.FullName, $content)
+	# Cycle over locale folders
 	foreach ($folder in $locale_folders) {
+		# Copy resulting .pot file as .po
 		Copy-Item $pot_file.FullName -Destination "$($folder.FullName)\LC_MESSAGES\$($pot_file.baseName).po"
 	}
 }
+
 echo "Press any key to finish..."
 $x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
