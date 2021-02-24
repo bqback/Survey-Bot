fa5b7c05c307cc727cc4745b07402676aaaee81d
Nik <lolwut-lol@yandex.ru>
2021-02-24 22:29:20 +0300

    Renamed custom class to comply with the ptbcontrib version

--- bot/extbotcommand.py
+++
@@ -1,9 +0,0 @@
-from telegram import BotCommand
-
-class ExtBotCommand(BotCommand):
-    def __init__(self, command: str, description: str, long_description: str = None):
-        super().__init__(command, description)
-        if long_description is not None:
-            self.long_description = long_description
-        else:
-            self.long_description = description
